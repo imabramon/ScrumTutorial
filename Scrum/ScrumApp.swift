e8fe3654139d183e437dec929c1760ef32d63b59
im_abramon <im_abramon@vk.com>
2021-10-27 17:37:02 +0300

    Initial Commit

---
+++ Scrum/ScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumApp.swift
+//  Scrum
+//
+//  Created by Влад Шелопугин on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
