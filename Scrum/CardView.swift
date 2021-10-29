341e95a9f30677636c95eec61c05e68ec45c056c
im_abramon <im_abramon@vk.com>
2021-10-29 16:49:24 +0300

    Section 1 Step 6

---
+++ Scrum/CardView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
