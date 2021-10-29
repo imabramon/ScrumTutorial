53791206207427843a166f39c8648bee1d7fb88b
im_abramon <im_abramon@vk.com>
2021-10-29 17:13:53 +0300

    Section 2 Step 11

--- Scrum/CardView.swift
+++ Scrum/CardView.swift
@@ -6,7 +6,17 @@ struct CardView: View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
     }
 }
 
@@ -16,5 +26,6 @@ struct CardView_Previews: PreviewProvider {
         CardView(scrum: scrum)
             .background(scrum.color)
             .previewLayout(.fixed(width: 400, height: 60))
+            .preferredColorScheme(.dark)
     }
 }
