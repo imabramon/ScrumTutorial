530afb40f4ea1a0bad0a8ac313eedc093f769a14
im_abramon <im_abramon@vk.com>
2021-11-06 01:21:25 +0300

    MeetingFooter

--- Scrum/MeetingView.swift
+++ Scrum/MeetingView.swift
@@ -19,18 +19,18 @@ struct MeetingView: View {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, scrumColor: scrum.color)
                 Circle()
                     .strokeBorder(lineWidth: 24, antialiased: true)
-                HStack{
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}){
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel(Text("Next speaker"))
-                }
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
         .padding()
         .foregroundColor(scrum.color.accessibleFontColor)
+        .onAppear{
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear{
+            scrumTimer.stopScrum()
+        }
     }
 }
 
